9f3b2adff257bbe7cc639564f5cbc95d9dc82684
aandragion <aanmachrus@gmail.com>
2019-10-27 20:15:22 +0700

    tambah layoutbbb

---
+++ app/src/main/java/com/example/oke/dpemasukkan.java
@@ -0,0 +1,14 @@
+package com.example.oke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dpemasukkan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dpemasukkan);
+    }
+}
