83b2482dd284d6051d4b16b7af7379eb45baf44e
aandragion <aanmachrus@gmail.com>
2019-12-04 09:10:06 +0700

    04/12/2019 online1

--- app/src/main/java/com/example/oke/library/load.java
+++ app/src/main/java/com/example/oke/library/load.java
@@ -4,15 +4,15 @@ public class load {
     public static String foto(String photo){
         String hasil = "";
 
-        hasil = "http://192.168.8.109/admin/upload/gbrfilm/" +photo;
+        hasil = "https://cobabioskop.000webhostapp.com/upload/gbrfilm/" +photo;
 
         return hasil;
     }
 
     public static String video(String movie){
         String hasil = "";
-
-        hasil = "http://192.168.8.109/admin/upload/vdfilm/" +movie;
+//http://192.168.8.109/admin/
+        hasil = "https://cobabioskop.000webhostapp.com/upload/vdfilm/" +movie;
 
         return hasil;
     }
@@ -20,7 +20,7 @@ public class load {
     public static String gambar(String gambar){
         String hasil = "";
 
-        hasil = "http://192.168.8.109/admin-api/gambar/" +gambar;
+        hasil = "https://cobabioskop.000webhostapp.com/gambar/" +gambar;
 
         return hasil;
     }
